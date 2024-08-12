0d6aabd2083fb05b459107ce4bae14982e975d9a
oshada20056 <153474645+oshada20056@users.noreply.github.com>
2024-08-12 19:53:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     DATABASE_URL = os.environ.get('DATABASE_URL')
   else:
     BOT_TOKEN = '6234064473:AAE8cTNtmnVuwEYZKZCmMdCn4KUW0FwBJN0' # Get it from https://t.me/BotFather
-    APP_ID = '1680362' # Get it from my.telegram.org/apps
-    API_HASH = '8cebfa99b9719c28a5e16fedaa0eeaad' # Get it from my.telegram.org/apps
+    APP_ID = '21204722' # Get it from my.telegram.org/apps
+    API_HASH = '4f5b4bbc15e7f9df9961ac92e8fd219b' # Get it from my.telegram.org/apps
     DATABASE_URL = 'postgres://mdvgqgverqgesc:263a6b5b49f8539c4b1bb106ff7b300e5b5ebb3b46c3314fc57e8da9865c6457@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d6c18273hu4a0r' # SQL Database URL / Heroku Postgres URL
 
 
